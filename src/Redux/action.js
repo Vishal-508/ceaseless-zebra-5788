5109fa2e20c4a1c5bedf7f45338ecbd708aee204
vishal sharma <vishalsharma331441@gmail.com>
2022-09-27 17:21:11 +0530

    first

---
+++ src/Redux/action.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const action = () => {
+  return (
+    <div>action</div>
+  )
+}
+
+export default action
---
+++ src/Redux/actionTypes.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const actionTypes = () => {
+  return (
+    <div>actionTypes</div>
+  )
+}
+
+export default actionTypes
