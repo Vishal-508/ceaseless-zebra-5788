da675f17af054e3aa6122845b4f87c423ae4c11e
aamirkhan9420 <aamirkhan.z9373@gmail.com>
2022-12-24 15:40:58 +0530

    day2 conde updated

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,24 +1,24 @@
-import { Box ,Flex,Text} from '@chakra-ui/react'
+import { Box ,Flex,SimpleGrid,Text} from '@chakra-ui/react'
 import React from 'react'
-
+import style from './Footer.module.css'
 export default function Footer() {
   return (
-    <div>
-        <Flex display={"flex"} alignItems={"center"} w={"100%"}  justifyContent={"space-evenly"} backgroundColor={"black"}  >
-            <Box display={"flex"} alignItems={"center"} justifyContent={"space-arround"} p={5}>
+   <div className={style.container}>
+        <Box display={"flex"} alignItems={"center"}  justifyContent={"space-between"} backgroundColor="black" w={"100%"} p={2}>
+            <SimpleGrid columns={{base:1,md:2,lg:5,}}   p={2} fontSize={{base:20,md:25,lg:18}} w={{base:"40%",md:"50%",lg:"50%"}}>
                 <Text color={"white"}>Terms of Use|</Text>
                 <Text color={"white"}>Privacy Policy|</Text>
-                <Text color={"white"}>Interest-Based Ads|</Text>
-                <Text color={"white"}>Addendum to the Global Privacy Policy|</Text>
+               
+               
                 <Text color={"white"}>Feedback|</Text>
-            </Box>
-            <Box>
+            </SimpleGrid>
+            <Box fontSize={{base:20,md:16,lg:18}} pr={5}>
                 <Text color={"white"}>
                 © 2022 ESPN Sports Media Ltd. All rights reserved
                 </Text>
             </Box>
 
-        </Flex>
-    </div>
+        </Box>
+  </div>
   )
 }
